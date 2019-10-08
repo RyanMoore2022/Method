1608bb57a638f80b8e8788214ff933286b64141e
Ryan Moore <ryan.moore.2022@student.lhschools.org>
2019-10-08 10:00:48 -0400

    return hello + word

--- src/methods.java
+++ src/methods.java
@@ -1,12 +1,20 @@
+import java.util.Scanner;
+
 public class methods {
 
     public static void main(String[] args){
 
     System.out.println("Hello World");
-
     }
 
-    public static
+    public static String hello(){
+        String hello = "Hello ";
+        System.out.println("Hello?");
+        Scanner keyboard = new Scanner(System.in);
+        String word;
+        word = keyboard.next();
+        return hello+word;
+    }
 
 
 
